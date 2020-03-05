f9c4932d3348a969824cecaefc617697f46f8c67
simphiwedev <seesimbarun@gmail.com>
2020-03-05 10:19:30 +0200

    updated the backend token & redirect url

--- src/App.js
+++ src/App.js
@@ -80,7 +80,6 @@ class App extends Component {
     let accessToken = parsed.access_token;
     if (!accessToken)
       return;
-    
     fetch('https://api.spotify.com/v1/me', {
       headers: {'Authorization': 'Bearer ' + accessToken}
     }).then((response) => response.json())
