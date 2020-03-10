e76bbd63781f7456f8e4e5c01763c22310aa950b
simphiwedev <seesimbarun@gmail.com>
2020-03-10 12:51:02 +0200

    Revert "recovered the App.js"
    This reverts commit b3d8bcda24d4d895ff24bbaf3a159d0d8dc3bc3e.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ let defaultStyle = {
   color: '#fff'
 };
 
-/* declared variable, has a user that holds parameter (name) */
-
 class PlaylistCounter extends Component {
   render() {
     return (
@@ -20,17 +18,17 @@ class PlaylistCounter extends Component {
 
 class HoursCounter extends Component {
   render() {
-		let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
-			return songs.concat(eachPlaylist.songs)
-		}, [])
-		let totalDuration  = allSongs.reduce((sum, eachSong) => {
-			return sum + eachSong.duration
-		}, 0)
-
-		return (
-			<div style={{...defaultStyle, width: '40%', display: 'inline-block', fontSize: '22px'}}>
-				<h2>{Math.round(totalDuration/1000)} Hours</h2>
-			</div>
+    let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
+      return songs.concat(eachPlaylist.songs)
+    }, [])
+    let totalDuration  = allSongs.reduce((sum, eachSong) => {
+      return sum + eachSong.duration
+    }, 0)
+    
+    return (
+      <div style={{...defaultStyle, width: '40%', display: 'inline-block', fontSize: '22px'}}>
+        <h2>{Math.round(totalDuration/1000)} Hours</h2>
+      </div>
     );
   }
 }
